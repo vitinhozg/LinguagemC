f6ab585c2bd49964b7aa2d287e744aab8087b05e
Vitor Ferreira <vitinhozg16@hotmail.com>
2022-03-18 09:40:03 -0300

    Cargos e salarios

---
+++ 4-16032022/cargos_switch.c
@@ -0,0 +1,51 @@
+/*
+Programas para exibir o salário do 
+cargo de acordo com a escolha do 
+usuário
+*/
+#include <stdio.h>
+
+int main(){
+    
+    int opcao = 0;
+
+    //comando para limpar a tela
+    system("clear");//clear screen(limpar tela)
+    printf("########################################################");
+    printf("\n#\t Programa para exibir salário\t\t\t#\n");
+    printf("########################################################\n");
+    printf("\nEscolha um dos cargos abaixo digitando o número\n");
+    printf("\t1 - Diretor\n ");
+    printf("\t2 - Gerente\n");
+    printf("\t3 - Analista\n");
+    printf("\t4 - Assistente\n");
+    printf("\t5 - Auxiliar");
+    printf("-----------------------------------------------------------------\n");
+
+    scanf("%d", &opcao);
+
+    switch(opcao){
+        case 1:
+            printf("O salário do Diretor é R$ 15.000,00");
+        break;
+        
+        case 2:
+            printf("O salário do Gerente é R$ 12.000,00\n");
+        break;
+        case 3:
+            printf("O salário do Analista é R$ 8.000,00\n");
+        break;
+        case 4:
+            printf("O salário do Assistente é R$ 4.000,00\n");
+        break;
+        case 5:
+            printf("O salario do Auxiliar é R$ 2.000,00\n");
+        break;
+        default:
+            printf("Não há salário\n");
+        break;
+
+    }
+
+    return 0;
+}
---
+++ 5-17032022/tabuada.c
@@ -0,0 +1,18 @@
+/*
+Programa para calcular a tabuada de
+acordo com a entrada do usuário
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    int mult;
+    printf("Digite um número para a tabuada\n");
+    scanf("%d",&n);
+
+    for(mult = 0 ; mult <= 10 ; mult++ ){
+        printf("%d x %d = %d\n",n,mult,(n*mult));
+
+    }
+    
+    return 0;
+}
