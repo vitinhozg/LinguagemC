5370556705d6488fc622e4ae202a3ba43cac88c1
vitinhozg <101403193+vitinhozg@users.noreply.github.com>
2022-03-28 08:48:33 -0300

    Add files via upload

---
+++ 9-25032022/ponteiro1.c
@@ -0,0 +1,12 @@
+/*
+Programa para estudar um pouco sobre ponteiro.
+Este sera o nosso primeiro exemplo
+*/
+#include <stdio.h>
+#include <stdlib.h>
+void main(){
+
+    int valor = 50;
+    int *pvalor = &valor;
+    printf("%d é o valor e a posição em memória é %p\n",valor,pvalor);
+}
