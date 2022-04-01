b1613ad3461d818ab03652b2c91f559cf4375ddb
vitinhozg <101403193+vitinhozg@users.noreply.github.com>
2022-04-01 09:13:18 -0300

    Add files via upload

---
+++ 11-31032022/array2.c
@@ -0,0 +1,14 @@
+/*
+Programa que reliza a soma usando um array 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "calculos.h"
+
+void main(){
+    int v[10] = {1,23,52,4,0,1,2,5,8,10};
+    soma(v,10);
+    maiorValor(v,10);
+    menorValor(v,10);
+    ordenar(v,10);
+}
